9e17e7befc8ca45a3f9f694bba5d55c598cdd639
Andres <andres-a23@live.com>
2020-11-22 14:49:40 -0500

    starting trees

---
+++ Trees/Trees.js
@@ -0,0 +1,13 @@
+// Perfect Binary Tree
+    // Has all leaf nodes are full
+    // No node that has one child its either 0 or more than one
+    // The amount of nodes in each level are doubled
+
+
+// Full Binary Tree
+    // A node has either 0 or 2 children but never one child
+
+    // Binary Search Tree
+        // lookup = 0(log N)
+        // insert = 0(log N)
+        // delete = 0(log N)
