60572466597e039b42b73530dfd75db9373d723a
Andres <andres-a23@live.com>
2020-11-22 00:10:18 -0500

    creating new linked list

--- LinkedLists/linkedList.js
+++ LinkedLists/linkedList.js
@@ -1,1 +1,17 @@
 // Linked Lists SINGLY and DOUBLY
+// An object that includes object that point to other objects within the class
+
+// pros
+    // 
+
+// Cons
+    // 
+
+// Time Complexity
+    // prepend - O(1)
+    // append - O(1)
+    // lookup - O(n)
+    // inset - O(n)
+    // delete - O(n)
+
+// Pointer - a reference to another point in memory
