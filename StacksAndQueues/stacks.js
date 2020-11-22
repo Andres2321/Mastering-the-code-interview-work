42c7f38c95353c3d0d82c1694ccec601099edd71
Andres <andres-a23@live.com>
2020-11-22 09:57:03 -0500

    starting stacks

---
+++ StacksAndQueues/stacks.js
@@ -0,0 +1,12 @@
+// Stacks
+    // An array or list of function calls and parameters.
+
+
+// Time Complexity
+    // lookup = O(n)
+    // pop = O(1)
+    // push = O(1)
+    // peek = O(1)
+
+// LIFO
+    // Last In First Out
