70834190c408af2c6d9adc2b0e482b67eceee371
Andres <andres-a23@live.com>
2020-11-21 22:35:48 -0500

    created a set and get function for my hash table

--- HashTables/creatingHashTable.js
+++ HashTables/creatingHashTable.js
@@ -1,16 +1,43 @@
 class HashTable {
     constructor(size){
-        jthis.data = new Array(size)
+        this.data = new Array(size)
     }
+
     _hash(key){
         let hash = 0
         for(let i = 0; i < key.length; i++){
-            has = (hash + key.charCodeAt(i) * i) %
+            hash = (hash + key.charCodeAt(i) * i) %
             this.data.length
         }
         return hash
     }
+
+    // TC = O(1)
+    set(key, value){ 
+        let address = this._hash(key);
+        // If we do not have anything in this space, add the following...
+        if(!this.data[address]){
+            this.data[address] = []
+            this.data[address].push([key,value])
+            console.log(this.data)
+        } 
+        // will add onto the collision array
+        this.data[address].push([key,value])
+    }
+    // TC = O(1)
+    get(key){
+        let address = this._hash(key)
+        const currentBucket = this.data[address]
+
+        if(currentBucket) {
+            for (let i = 0; i < currentBucket.length; i++){
+                if (currentBucket[i][0] === key) currentBucket[i][1]
+            }
+        }
+        return null
+    }
 }
 
-
 const myHashTable = new HashTable(50)
+myHashTable.set('grapes', 10000)
+myHashTable.get('grapes')
