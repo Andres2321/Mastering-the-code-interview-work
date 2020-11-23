362c13351e51cd2a4a3d6c95ceafcbbd9e23f7df
Andres <andres-a23@live.com>
2020-11-22 21:38:04 -0500

    finished data structure section

---
+++ Graph/graph.js
@@ -0,0 +1,23 @@
+// Graph
+    // Each item is a node or a vertex
+    // Node are connected with edges
+    // Create relationships
+    // Scaling is difficult
+
+// Directed graph
+    // One way direction
+
+// Undirected Graph
+    // Multiple direction
+
+// Weighted graphs
+    // can have elements in edges
+
+// Unweighted graphs
+
+// Cyclic
+    // When you can go to one node to another and circle back
+
+// Acyclic
+    // No way to return to starting node
+
