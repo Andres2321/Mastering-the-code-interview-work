cdcf3ad96a7e2bbacba1d6748c9d1009f6fa4846
Andres <andres-a23@live.com>
2020-11-22 21:29:24 -0500

    finalizing creation of graph

---
+++ Trees/binaryHeap.js
@@ -0,0 +1,20 @@
+// Binary Heap
+    // Every node on the top level have a larger value than lower levels
+    // Great at comparative operations
+    // Will shift values if values is inserted and is largest variable in heap.
+        // Worst case: Object(log n)
+        // Best case: O(1)
+
+// Time Consplexity
+    // Lookup - O(n)
+    // insert - O(log N)
+    // delete - O(log N)
+
+    // Pros
+        // better than O(n)
+        // priority
+        // flexible size
+        // fast insert
+
+    // Coms
+        // slow lookup
---
+++ Trees/trie.js
@@ -0,0 +1,4 @@
+// Trie
+    // Specialized in searching for text
+
+// Time Complexity = O(length of the word)
